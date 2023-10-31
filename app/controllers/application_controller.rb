86edbf6176e80eaa0d01861b61f4a058de29b82d
yurikomura <kafuu1313@gmail.com>
2023-10-31 19:41:24 +0900

    ユーザー管理機能実装１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name,:last_name_kana, :first_name_kana,:birth_date])
+  end
   before_action :basic_auth
 
   private
@@ -9,3 +15,5 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,126 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it '全ての入力事項が、存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+
+      it 'パスワードが6文字以上半角英数字であれば登録できる' do
+        @user.password = '123abc'
+        @user.password_confirmation = '123abc'
+        expect(@user).to be_valid
+      end
+
+      it '名字が全角(漢字・ひらがな・カタカナ)であれば登録できる' do
+        @user.last_name = '山田'
+        expect(@user).to be_valid
+      end
+
+      it '名前が全角(漢字・ひらがな・カタカナ)であれば登録できる' do
+        @user.first_name  = '陸太郎'
+        expect(@user).to be_valid
+      end
+
+      it '名字のフリガナが全角(カタカナ)であれば登録できる' do
+        @user.last_name_kana = 'ヤマダ'
+        expect(@user).to be_valid
+      end
+
+      it '名前のフリガナが全角(カタカナ)であれば登録できる' do
+        @user.first_name_kana = 'リクタロウ'
+        expect(@user).to be_valid
+      end
+    end
+
+
+
+    context '新規登録できない場合' do
+      it 'ニックネームが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Nickname can't be blank")
+      end
+
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include ("Email can't be blank")
+      end
+
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+
+      it "passwordが空では登録できない" do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = 'ab123'
+        @user.password_confirmation = 'ab123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'passwordが半角英数字でないと登録できない' do
+        @user.password = 'あ123abc'
+        @user.password_confirmation = 'あ123abc'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '名字が全角(漢字・ひらがな・カタカナ)でないと登録できない' do
+        @user.last_name = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name 全角文字を使用してください")
+      end
+
+      it '名前が全角(漢字・ひらがな・カタカナ)でないと登録できない' do
+        @user.first_name = 'rikutaro'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name 全角文字を使用してください")
+      end
+
+      it '名字のフリガナが全角(カタカナ)でないと登録できない' do
+        @user.last_name_kana = 'やまだ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana 全角カナを使用してください")
+      end
+
+      it '名前のフリガナが全角(カタカナ)でないと登録できない' do
+        @user.first_name_kana = 'りくたろう'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana 全角カナを使用してください")
+      end
+
+      it '生年月日が空欄だと登録できない' do
+        @user.birth_date = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      end
+    end
+  end
+end
