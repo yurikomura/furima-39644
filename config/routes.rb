c616b0b30974d5571b04a3ba31d2a02ed3a96d8d
yurikomura <kafuu1313@gmail.com>
2023-11-05 21:29:08 +0900

    出品した商品の情報をテーブルに保存

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: "items#index"
   resources :items do
-    resources :items, only: [:index, :create]
+    resources :items, only: [:index, :new, :create]
    end
 end
