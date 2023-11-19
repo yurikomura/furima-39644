7aba81bcd6c56b314744ee8e6410591af9821aa2
yurikomura <kafuu1313@gmail.com>
2023-11-19 20:03:03 +0900

    エラーメッセージを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,30 @@
 class OrdersController < ApplicationController
+  #before_action :authenticate_user!
+
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id:current_user.id,item_id:params[:item_id])
   end
 end
+
+
+
