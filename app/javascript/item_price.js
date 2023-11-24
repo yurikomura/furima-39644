928aa424e9ec6cbcba6aa437b89fdac33a687c23
yurikomura <kafuu1313@gmail.com>
2023-11-24 21:28:36 +0900

    data: { turbo: false }追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
+  console.log("OK")
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
