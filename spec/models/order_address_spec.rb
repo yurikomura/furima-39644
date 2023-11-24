9cca8be5e97274e6ef19309d5bf8578d33d69483
yurikomura <kafuu1313@gmail.com>
2023-11-24 19:58:43 +0900

    修正３

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    @order_address = FactoryBot.build(:order_address)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
   end
 
   describe '商品購入情報の保存' do
@@ -73,8 +75,7 @@ RSpec.describe OrderAddress, type: :model do
       it '電話番号が空だとと保存できない' do
         @order_address.phone_number = nil
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number can't be blank",
-                                                               'Phone number is too short (minimum is 10 characters)', 'Phone number should be 10 to 11 digits')
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
       it '電話番号にハイフンがあると保存できない' do
         @order_address.phone_number = '123-4567-8910'
